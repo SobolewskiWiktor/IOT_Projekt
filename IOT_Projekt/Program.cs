6f54587e68183738880c8006967382f87a5890f1
SobolewskiWiktor <sobolsstudio@gmail.com>
2024-05-15 14:58:42 +0200

    Start working on controller

--- IOT_Projekt/Program.cs
+++ IOT_Projekt/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Azure.Devices;
+string conString = File.ReadAllText("AzureConString.txt");
+Console.WriteLine("============================================");
+Console.WriteLine("[Controller] Azure Conntecting String Loaded");
+
+using var serviceClient = ServiceClient.CreateFromConnectionString(conString);
+using var registryManager = RegistryManager.CreateFromConnectionString(conString);
+
+var manager = new IotHubManager(serviceClient, registryManager);
+
+int input;
+do
+{
+    FeatureSelector.PrintMenu(); 
+    input = FeatureSelector.ReadInput(); 
+    await FeatureSelector.Execute(input, manager); 
+} while (input != 0);
+
+Console.WriteLine("[Controller] Controller End Working");
+Console.WriteLine("============================================");
