0e4894b7be64a9460d188eccc34ba05af96e09a1
SobolewskiWiktor <sobolsstudio@gmail.com>
2024-05-15 15:12:12 +0200

    Start working on device agent

--- IOT_Project_Device/Program.cs
+++ IOT_Project_Device/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices;
+
+
+string conString = File.ReadAllText(@"ConnectionString.txt");
+Console.WriteLine("Connection String loaded");
+
+using var deviceClient = DeviceClient.CreateFromConnectionString(conString);
+await deviceClient.OpenAsync();
+var device = new Device1(deviceClient);
+Console.WriteLine("Connection with Device Established");
+await device.InitializerHandlers();
+await device.UpdateTwinAsync();
+Console.WriteLine("Ready to WORK =)");
+await device.SendMessages(2, 1000);
+var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+while (await periodicTimer.WaitForNextTickAsync())
+{
+    await device.TimerSendingMessages();
+    //Console.WriteLine("Waiting 10seconds to send next msg");
+}
+//Console.ReadLine();
